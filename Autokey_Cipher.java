94260a96a2ac52ed01a0eb21ea3b986bf2fcc60c
21CEUOS101 <120437060+21CEUOS101@users.noreply.github.com>
2023-12-20 09:30:45 +0000

    lab3 nis

---
+++ Autokey_Cipher.java
@@ -0,0 +1,20 @@
+public class Autokey_Cipher {
+
+    public static String encrypt(String text , int key){
+        int[] enc = new int[text.length()];
+        enc[0] = key;
+
+        for(int i=1;i<enc.length;i++)
+        {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "hello";
+        int key = 5;
+
+
+    }
+    
+}
---
+++ Playfair_Cipher.java
@@ -0,0 +1,123 @@
+public class Playfair_Cipher{
+
+    public static int[][] gen_mat(String key)
+    {
+        StringBuilder s = new StringBuilder(key);
+        String abcd = "abcdefghijklmnopqrstuvwxyz";
+        boolean flag = true;
+
+        for(Character i : abcd.toCharArray())
+        {
+            for(Character j : s.toString().toCharArray())
+            {
+                if(j==i)
+                {
+                    flag = false;
+                }
+            }
+            if(flag && i!='j')
+            {
+                s = s.append(String.valueOf(i));
+            }
+            flag = true;
+        }
+
+        int[][] mat = new int[5][5];
+        int k=0;
+
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                mat[i][j] =(int) s.charAt(k);
+                k++;
+            }
+        }
+
+        return mat;
+    }
+
+    public static String encrypt(String text , int[][] mat)
+    {
+        String updated = text;
+
+        if(text.length() % 2 == 1)
+        {
+            text = text + "x";
+        }
+
+        System.out.println(text);
+
+        for(int i=0;i<text.length()-1;i+=2)
+        {
+            while(text.charAt(i) == 'j')
+            {
+                i++;
+            }
+            Character c1 = text.charAt(i);
+
+            int t = i+1;
+            while(text.charAt(t) == 'j')
+            {
+                t++;
+            }
+
+            Character c2 = text.charAt(t);
+
+            if(c2 == c1)
+            {
+                c2 = 'x';
+            }
+
+            int e1x = 0, e1y = 0 , e2x = 0 , e2y = 0;
+
+            for(int j=0;j<5;j++)
+            {
+                for(int k=0;k<5;k++)
+                {
+                    if(mat[j][k] == ((int) c1))
+                    {
+                        e1x = j;
+                        e1y = k;
+                    }
+                    else if(mat[j][k] == ((int) c2))
+                    {
+                        e2x = j;
+                        e2y = k;
+                    }
+                }
+            }
+
+            if(e1x == e2x)
+            {
+                e1y++;
+                e2y++;
+            }
+            else if(e1y == e2y)
+            {
+                e1x++;
+                e2x++;
+            }
+            else{
+                int temp = e1y;
+                e1y = e2y;
+                e2y = temp;
+            }
+
+            updated = updated.substring(0, i) + ((char) mat[e1x][e1y]) + updated.substring(i);
+            updated = updated.substring(0, t) + ((char) mat[e2x][e2y]) + updated.substring(t);
+        }
+
+        return updated;
+    }
+    public static void main(String[] args) {
+
+        int[][] mat = gen_mat("orange");
+
+        String text = "apple";
+        
+        String encrypted = encrypt(text , mat);
+
+        System.out.println(encrypted.substring(0,encrypted.length()-text.length()));
+    }
+}
